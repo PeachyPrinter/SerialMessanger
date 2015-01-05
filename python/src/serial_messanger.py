91e6e18e50df68b239e3f4dba7b4be36035e8a20
james townley <james@townley.ca>
2015-01-05 09:50:52 -0600

    Added handshake ability to api

--- python/src/serial_messanger.py
+++ python/src/serial_messanger.py
@@ -11,9 +11,12 @@ Usage:
 class SerialMessanger(threading.Thread):
     '''Initialize the class with a started connection matching the pyserialAPI (http://pyserial.sourceforge.net/pyserial_api.html)
     Note: You will need to manage the starting and stopping of this connection outside of this class'''
-    def __init__(self, connection):
+    def __init__(self, connection, header="a", footer="b", handshake="c"):
         threading.Thread.__init__(self)
         self.connection = connection
+        self._header = header
+        self._footer = footer
+        self._handshake = handshake
 
     '''Registers a handler and arg types for a specific message id.
     message_id - Message ids should be an integer between 0 and 255 (inclusive)
@@ -26,10 +29,20 @@ class SerialMessanger(threading.Thread):
         if message_id < 0 or message_id > 255:
             raise Exception('Message Id must be a integer between 0 and 255 (inclusive)')
 
+    def _do_handshake(self):
+        if self._handshake:
+            handshake = self._header + self._handshake + self._footer
+            self.connection.write(handshake)
+            recieved = ''
+            while not handshake in recieved:
+                read = self.connection.read(1)
+                recieved += read
+
     def run(self):
-        self._running = True
         self.connection.flushInput()
         self.connection.flushOutput()
+        self._do_handshake()
+        self._running = True
         while(self._running):
             pass
 
--- python/test/serial_messanger_test.py
+++ python/test/serial_messanger_test.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import os
-from mock import patch, Mock
+from mock import Mock
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
@@ -14,6 +14,8 @@ class SerialMessangerTest(unittest.TestCase):
 
     def setUp(self):
         self.test_serial_messanger = None
+        self.mock_connection = Mock()
+        self.test_serial_messanger = SerialMessanger(self.mock_connection, handshake = None)
 
     def tearDown(self):
         if self.test_serial_messanger:
@@ -22,8 +24,6 @@ class SerialMessangerTest(unittest.TestCase):
                 self.test_serial_messanger.join(1000)
 
     def test_register_id_must_be_between_0_and_255(self):
-        mock_connection = Mock()
-        self.test_serial_messanger = SerialMessanger(mock_connection)
         with self.assertRaises(Exception):
             self.test_serial_messanger.register(-1, self.call_back, type(123))
         with self.assertRaises(Exception):
@@ -31,9 +31,7 @@ class SerialMessangerTest(unittest.TestCase):
 
         self.test_serial_messanger.register(1, self.call_back, type(123))
 
-    def test_starts_and_stops(self ):
-        mock_connection = Mock()
-        self.test_serial_messanger = SerialMessanger(mock_connection)
+    def test_starts_and_stops(self):
         self.test_serial_messanger.start()
         self.assertEquals(True, self.test_serial_messanger.is_alive())
         self.test_serial_messanger.close()
@@ -41,13 +39,31 @@ class SerialMessangerTest(unittest.TestCase):
         self.test_serial_messanger.join(1000)
 
     def test_start_flushes_connection(self):
-        mock_connection = Mock()
-        self.test_serial_messanger = SerialMessanger(mock_connection)
         self.test_serial_messanger.start()
         self.test_serial_messanger.close()
         self.test_serial_messanger.join(1000)
-        mock_connection.flushInput.assert_called_with()
-        mock_connection.flushOutput.assert_called_with()
+        self.mock_connection.flushInput.assert_called_with()
+        self.mock_connection.flushOutput.assert_called_with()
+
+    def test_start_should_transmit_handshake_if_provided_and_wait_for_a_response(self):
+        expected_header = 'aaa'
+        expected_footer = 'ccc'
+        expected_handshake = 'hello'
+        send_bytes = expected_header + expected_handshake + expected_footer
+        return_bytes = list(send_bytes)
+
+        def side_effect(self):
+            if bytes:
+                return return_bytes.pop(0)
+            else:
+                raise Exception('test failure read too many bytes')
+        self.mock_connection.read.side_effect = side_effect
+        self.test_serial_messanger = SerialMessanger(self.mock_connection,header=expected_header, footer=expected_footer, handshake=expected_handshake)
+        self.test_serial_messanger.start()
+
+        self.test_serial_messanger.close()
+        self.test_serial_messanger.join(1000)
+        self.mock_connection.write.assert_called_with(send_bytes)
 
 
 if __name__ == '__main__':
