b10b0439b12ac57f62c2635a5f3866afd9e6f797
James Townley <software@peachyprinter.com>
2015-01-05 15:02:45 -0600

    Basics of processing messages started

--- python/src/serial_messanger.py
+++ python/src/serial_messanger.py
@@ -10,12 +10,12 @@ Usage:
 
 class SerialMessanger(threading.Thread):
     '''Initialize the class with a started connection matching the pyserialAPI (http://pyserial.sourceforge.net/pyserial_api.html)
-    Notes: 
+    Notes:
         You will need to manage the starting and stopping of this connection outside of this class'''
     def __init__(self,
                  connection,
-                 header="a",
-                 footer="b",
+                 header='HEAD',
+                 footer='FOOT',
                  handshake="c",
                  handshake_timeout_sec=3):
         threading.Thread.__init__(self)
@@ -26,9 +26,11 @@ class SerialMessanger(threading.Thread):
         self.footer = footer
         self.handshake = handshake
         self.handshake_timeout_sec = handshake_timeout_sec
+        self._read_chuck_size = 10
 
         self._running = False
         self._connection_failure = None
+        self._registered_messages = {}
 
     '''Registers a handler and arg types for a specific message id.
     message_id - Message ids should be an integer between 0 and 255 (inclusive)
@@ -36,11 +38,8 @@ class SerialMessanger(threading.Thread):
     types      - A list of ordered c-types (struct-like) corrosponding to the callback parameters'''
     def register(self, message_id, callback, types):
         self._is_valid_id(message_id)
-        try:
-            struct.calcsize(types)
-        except Exception as ex:
-            raise ex
-
+        self._is_valid_types(types)
+        self._registered_messages[str(message_id)] = (types, callback)
 
     '''Shutdowns thread'''
     def close(self):
@@ -60,6 +59,14 @@ class SerialMessanger(threading.Thread):
         if message_id < 0 or message_id > 255:
             raise Exception('Message Id must be a integer between 0 and 255 (inclusive)')
 
+    def _is_valid_types(self, types):
+        for special in ['<', '>', '@', '=', '!']:
+            assert(not special in types)
+        try:
+            struct.calcsize(types)
+        except Exception as ex:
+            raise ex
+
     def _do_handshake(self):
         if self.handshake:
             timeout = time.time() + self.handshake_timeout_sec
@@ -73,15 +80,30 @@ class SerialMessanger(threading.Thread):
                 self._connection_failure = "Handshake Timed Out"
                 raise Exception("Handshake Timed Out")
 
+    def _process_data(self, data):
+        message_id = str(struct.unpack('!h', data[:2])[0])
+        if message_id in self._registered_messages:
+            types, callback = self._registered_messages[message_id]
+            fmt = '!' + types
+            data_tuple = struct.unpack(fmt, data[2:])
+            callback(*data_tuple)
+
     def run(self):
         self.connection.flushInput()
         self.connection.flushOutput()
         try:
             self._do_handshake()
             self._running = True
+            raw_data = ''
             while(self._running):
-                pass
-        except Exception:
-            pass
+                raw_data += self.connection.read(self._read_chuck_size)
+                header_index = raw_data.find(self.header)
+                if header_index >= 0:
+                    footer_index = raw_data[header_index:].find(self.footer)
+                    if footer_index > header_index:
+                        self._process_data(raw_data[header_index + len(self.header):footer_index])
+                        raw_data = raw_data[footer_index + len(self.footer):]
+        except Exception as ex:
+            print(ex)
         finally:
             self._running = False
--- python/test/serial_messanger_test.py
+++ python/test/serial_messanger_test.py
@@ -1,7 +1,8 @@
 import unittest
 import sys
 import os
-from mock import Mock
+import time
+from mock import MagicMock
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
@@ -9,12 +10,14 @@ from serial_messanger import SerialMessanger
 
 
 class SerialMessangerTest(unittest.TestCase):
-    def call_back(*args):
-        pass
+    def call_back(self, *args):
+        self.call_back_args = args
 
     def setUp(self):
+        self.time_to_wait_async = 0.5
+        self.call_back_args = None
         self.test_serial_messanger = None
-        self.mock_connection = Mock()
+        self.mock_connection = MagicMock()
         self.test_serial_messanger = SerialMessanger(self.mock_connection, handshake=None)
 
     def tearDown(self):
@@ -24,6 +27,7 @@ class SerialMessangerTest(unittest.TestCase):
                 self.test_serial_messanger.join(1)
 
     def test_starts_and_stops(self):
+        self.mock_connection.read.return_value = ''
         self.test_serial_messanger.start()
         self.assertEquals(True, self.test_serial_messanger.is_alive())
         self.test_serial_messanger.close()
@@ -31,6 +35,7 @@ class SerialMessangerTest(unittest.TestCase):
         self.test_serial_messanger.join(1)
 
     def test_start_flushes_connection(self):
+        self.mock_connection.read.return_value = ''
         self.test_serial_messanger.start()
         self.test_serial_messanger.close()
         self.test_serial_messanger.join(1)
@@ -45,13 +50,15 @@ class SerialMessangerTest(unittest.TestCase):
         return_bytes = list(send_bytes)
 
         def side_effect(self):
-            if bytes:
+            if return_bytes:
                 return return_bytes.pop(0)
             else:
-                raise Exception('test failure read too many bytes')
+                return ''
+
         self.mock_connection.read.side_effect = side_effect
         self.test_serial_messanger = SerialMessanger(self.mock_connection, header=expected_header, footer=expected_footer, handshake=expected_handshake)
         self.test_serial_messanger.start()
+        time.sleep(self.time_to_wait_async)
         self.test_serial_messanger.close()
         self.test_serial_messanger.join(1)
         self.mock_connection.write.assert_called_with(send_bytes)
@@ -64,6 +71,7 @@ class SerialMessangerTest(unittest.TestCase):
         self.test_serial_messanger = SerialMessanger(self.mock_connection, header=expected_header, footer=expected_footer, handshake=expected_handshake, handshake_timeout_sec=0.3)
         with self.assertRaises(Exception):
             self.test_serial_messanger.start()
+        time.sleep(self.time_to_wait_async)
         self.assertFalse(self.test_serial_messanger.is_alive())
 
     def test_register_id_must_be_between_0_and_255(self):
@@ -80,6 +88,22 @@ class SerialMessangerTest(unittest.TestCase):
         with self.assertRaises(Exception):
             self.test_serial_messanger.register(1, self.call_back, type(123))
         self.test_serial_messanger.register(1, self.call_back, 'b')
+
+    def test_register_types_should_not_accept_special_characters(self):
+        for character in ['<h', '>h', '@h', '=h', '!h']:
+            with self.assertRaises(Exception):
+                self.test_serial_messanger.register(1, self.call_back, character)
+
+    def test_when_data_for_registered_message_is_recieved_call_back_is_issued(self):
+        self.test_serial_messanger.register(1, self.call_back, 'l')
+        self.mock_connection.read.return_value = 'HEAD\x00\x01\x00\x00\x00\x17FOOT'
+        self.test_serial_messanger.start()
+        time.sleep(self.time_to_wait_async)
+        self.test_serial_messanger.close()
+        self.test_serial_messanger.join(1)
+
+        self.assertEquals((23,), self.call_back_args)
+
 
 if __name__ == '__main__':
     unittest.main()
