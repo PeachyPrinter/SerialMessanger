9a0fb8b0a8e37f6b36bc73c95e331b875f5182b6
james townley <james@townley.ca>
2015-01-05 08:44:11 -0600

    Adding more around the api

--- python/src/serial_messanger.py
+++ python/src/serial_messanger.py
@@ -1,4 +1,6 @@
 import struct
+import threading
+import time
 
 
 '''Tool for serial communication alling for registration and call back of methods based on input data
@@ -6,12 +8,14 @@ Usage:
 '''
 
 
-class SerialMessanger(object):
-
+class SerialMessanger(threading.Thread):
+    '''Initialize the class with a started connection matching the pyserialAPI (http://pyserial.sourceforge.net/pyserial_api.html)
+    Note: You will need to manage the starting and stopping of this connection outside of this class'''
     def __init__(self, connection):
-        pass
+        threading.Thread.__init__(self)
+        self.connection = connection
 
-    '''Registers a handler and arg types for a specific message id. 
+    '''Registers a handler and arg types for a specific message id.
     message_id - Message ids should be an integer between 0 and 255 (inclusive)
     callback   - Method taking the types as parameters
     types      - A list of ordered types corrosponding to the callback parameters'''
@@ -21,3 +25,15 @@ class SerialMessanger(object):
     def _is_valid_id(self, message_id):
         if message_id < 0 or message_id > 255:
             raise Exception('Message Id must be a integer between 0 and 255 (inclusive)')
+
+    def run(self):
+        self._running = True
+        self.connection.flushInput()
+        self.connection.flushOutput()
+        while(self._running):
+            pass
+
+    def close(self):
+        self._running = False
+        while self.is_alive():
+            time.sleep(0.1)
--- python/test/serial_messanger_test.py
+++ python/test/serial_messanger_test.py
@@ -12,15 +12,42 @@ class SerialMessangerTest(unittest.TestCase):
     def call_back(*args):
         pass
 
+    def setUp(self):
+        self.test_serial_messanger = None
+
+    def tearDown(self):
+        if self.test_serial_messanger:
+            if self.test_serial_messanger.is_alive():
+                self.test_serial_messanger.close()
+                self.test_serial_messanger.join(1000)
+
     def test_register_id_must_be_between_0_and_255(self):
         mock_connection = Mock()
-        test_serial_messanger = SerialMessanger(mock_connection)
+        self.test_serial_messanger = SerialMessanger(mock_connection)
         with self.assertRaises(Exception):
-            test_serial_messanger.register(-1, self.call_back, type(123))
+            self.test_serial_messanger.register(-1, self.call_back, type(123))
         with self.assertRaises(Exception):
-            test_serial_messanger.register(256, self.call_back, type(123))
+            self.test_serial_messanger.register(256, self.call_back, type(123))
 
-        test_serial_messanger.register(1, self.call_back, type(123))
+        self.test_serial_messanger.register(1, self.call_back, type(123))
+
+    def test_starts_and_stops(self ):
+        mock_connection = Mock()
+        self.test_serial_messanger = SerialMessanger(mock_connection)
+        self.test_serial_messanger.start()
+        self.assertEquals(True, self.test_serial_messanger.is_alive())
+        self.test_serial_messanger.close()
+        self.assertEquals(False, self.test_serial_messanger.is_alive())
+        self.test_serial_messanger.join(1000)
+
+    def test_start_flushes_connection(self):
+        mock_connection = Mock()
+        self.test_serial_messanger = SerialMessanger(mock_connection)
+        self.test_serial_messanger.start()
+        self.test_serial_messanger.close()
+        self.test_serial_messanger.join(1000)
+        mock_connection.flushInput.assert_called_with()
+        mock_connection.flushOutput.assert_called_with()
 
 
 if __name__ == '__main__':
